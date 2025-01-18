389f3148e457ce3030a15fd73f1b1490039291bb
junyong0225 <junyong0225@gmail.com>
2025-01-18 11:24:41 +0900

    day1

---
+++ src/day1/Day1Print.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Day1Print {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+        System.out.print("Hello world\n");
+    }
+}
