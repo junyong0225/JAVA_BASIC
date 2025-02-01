16503f5df92913b90cee27933691a0589d9bb05d
junyong0225 <junyong0225@gmail.com>
2025-02-01 11:39:20 +0900

    day3

---
+++ src/day3/Day3Practice.java
@@ -0,0 +1,24 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Day3Practice {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        double result = (double) (num1 + num2 + num3) / 3;
+        result = Math.round(result);
+
+        System.out.println("average: " + result);
+
+
+//        double d = 3.5;
+//        int t = (int) d;
+//        System.out.println("t = " + t);
+    }
+}
