9cb01b50eca237d9057cf4808de731067a6893cd
junyong0225 <junyong0225@gmail.com>
2025-03-01 11:54:31 +0900

    day5

---
+++ src/day5/Day5Practice.java
@@ -0,0 +1,49 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Day5Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 1번
+       // for (int i = 1; i <= 10; i++) {
+         //   System.out.println("number" + i);
+        //}
+        // 2번
+        //for (int i = 1; i <= 10; i += 2) {
+           // System.out.println("number" + i);
+       // }
+        // 3번
+//        for (int i = 1; i <= 10; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//
+//        }
+        // 4번
+//        System.out.print("단을 입력하세요: ");
+//        int dan = sc.nextInt();
+//
+//        for (int i = 1; i <= 9; i++) {
+//            System.out.println(dan + " x " + i + " = " + (dan * i));
+//        }
+        // 5번
+//        System.out.print("단을 입력하세요: ");
+//        int dan = sc.nextInt();
+//
+//        for (int i = 9; i >= 1; i--) {
+//            System.out.println(dan + " x " + i + " = " + (dan * i));
+//        }
+
+        //6번
+        System.out.print("단을 입력하세요: ");
+        int dan = sc.nextInt();
+
+        for (int i = 1; i <= 18; i++) {
+            if (i == 3 || i == 6 || i == 7 || i == 9 || i == 18) {
+                System.out.println(dan + " x " + i + " = " + (dan * i));
+            }
+
+        }
+    }
+}
---
+++ src/day5/Day5Repeat.java
@@ -0,0 +1,27 @@
+package day5;
+
+public class Day5Repeat {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("안녕하세오" + i);
+        }
+
+        for (int i = 5; i >= 1; i--) {
+            System.out.println("안녕하세오" + i);
+        }
+
+    int count = 1;
+        while (count <= 5) {
+            System.out.println("count = " + count);
+            count++;
+        }
+
+    int countt = 5;
+        while (countt >= 1) {
+            System.out.println("counbt = " + countt);
+            countt--;
+        }
+    }
+}
